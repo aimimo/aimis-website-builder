4db03b873b09d861396a7b9d2e8ca544ad67ef3d
Aimi Meor <pam135@uclive.ac.nz>
2026-05-13 18:44:54 +1200

    Initial project setup: React client + Express server

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/my-current-status", (req, res) => {
+    res.json({status: "ok"});
+});
+
+app.listen(3001, () => {
+    console.log("Sheeeeeeeeeee's runnnninggg outttt the DOOOOOOOR http://localhost:3001")
+})
